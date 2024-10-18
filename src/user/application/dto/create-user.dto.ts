4614bc366dcadfe95dd48a60d86d5e41cee3082b
Wagner Reis <wagnerreis1997@gmail.com>
2024-10-18 20:34:28 -0300

    feat: add dtos

--- src/user/application/dto/create-user.dto.ts
+++ src/user/application/dto/create-user.dto.ts
@@ -1,1 +1,15 @@
 export class CreateUserDto {}
+
+export class UserPersistenceDTO {
+  id?: string;
+  name: string;
+  email: string;
+  password: string;
+}
+
+export class UserDTO {
+  id?: string;
+  name: string;
+  email: string;
+  password: string;
+}
