4fa60479a66a2564c73f88472d38965e3377b940
Wagner Reis <wagnerreis1997@gmail.com>
2024-10-18 19:08:12 -0300

    refactor: resome class name

--- src/@shared/notification/notification.error.ts
+++ src/@shared/notification/notification.error.ts
@@ -1,6 +1,6 @@
 import { NotificationErrorProps } from './notification';
 
-export default class NotifcationError extends Error {
+export default class NotificationError extends Error {
   constructor(public errors: NotificationErrorProps[]) {
     super(
       errors.map((error) => `${error.context}: ${error.message}`).join(','),
