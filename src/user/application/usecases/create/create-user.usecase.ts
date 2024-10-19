5b7e3b8aeaccc6a38392a5b01184bedc633e2fd3
Wagner Reis <wagnerreis1997@gmail.com>
2024-10-19 10:09:12 -0300

    feat: update CreateUserUseCase

--- src/user/application/usecases/create/create-user.usecase.ts
+++ src/user/application/usecases/create/create-user.usecase.ts
@@ -1,28 +1,46 @@
-import { HttpException, HttpStatus } from '@nestjs/common';
-import { UserDTO } from 'user/application/dto/create-user.dto';
+import { Injectable } from '@nestjs/common';
+import { UserDTO, UserOutput } from 'user/application/dto/create-user.dto';
 import { UserFactory } from 'user/domain/factories/user.factory';
 import { IUserRepository } from 'user/domain/repository/user.repository';
-import * as bcrypt from 'bcrypt';
+import { UserAlreadyExistsException } from 'user/domain/exceptions/user-exceptions';
+import { IPasswordService } from 'user/domain/services/password-service.interface';
 
+@Injectable()
 export class CreateUserUseCase {
-  constructor(private readonly userRepository: IUserRepository) {}
+  constructor(
+    private readonly userRepository: IUserRepository,
+    private passwordService: IPasswordService,
+  ) {}
 
-  async execute(user: UserDTO) {
-    const userAlreadExists = await this.userRepository.findByEmail(user.email);
+  async execute(user: UserDTO): Promise<UserOutput> {
+    await this.ensureUserDoesNotExists(user.email);
 
-    if (userAlreadExists) {
-      throw new HttpException('User already exists', HttpStatus.BAD_REQUEST);
-    }
+    const hash = await this.passwordService.hash(user.password);
 
     const newUser = UserFactory.createNew(user);
 
-    const saltOrRounds = 10;
-    const hash = await bcrypt.hash(newUser.password, saltOrRounds);
-
-    return await this.userRepository.create({
+    const savedUser = await this.userRepository.create({
       name: newUser.name,
       email: newUser.email,
       password: hash,
     });
+
+    return {
+      id: savedUser.id,
+      name: savedUser.name,
+      email: savedUser.email,
+      createdAt: savedUser.createdAt,
+      updatedAt: savedUser.updatedAt,
+    };
+  }
+
+  private async ensureUserDoesNotExists(
+    email: string,
+  ): Promise<void | UserAlreadyExistsException> {
+    const userAlreadExists = await this.userRepository.findByEmail(email);
+
+    if (userAlreadExists) {
+      throw new UserAlreadyExistsException(email);
+    }
   }
 }
