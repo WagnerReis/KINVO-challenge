aa7f59c6c3e2db8db556132de884bf0f435aebc3
Wagner Reis <wagnerreis1997@gmail.com>
2024-10-19 16:22:33 -0300

    refactor: add modules directory

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
-import { UserModule } from './user/application/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import mongodbConfig from './config/mongodb.config';
+import { UserModule } from './modules/user/application/user.module';
 
 @Module({
   imports: [
